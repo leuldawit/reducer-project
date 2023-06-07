f2d44e117e32d54338d32ba42043565a510beb59
eliyon <55352187+Eliyonqwe@users.noreply.github.com>
2023-06-07 20:19:11 +0300

    shredder animation and keyfacts stat added

--- plastic.js
+++ plastic.js
@@ -7,3 +7,31 @@ function openNav() {
   function closeNav() {
     document.getElementById("myNav").style.width = "0%";
   } 
+  
+  
+  const counters = Array.from(document.querySelectorAll(".nums"));
+const counterValues = [];
+const speed = 40;
+
+const updateCount = (counter, target, count, index) => {
+  const inc = target / speed;
+  counterValues[index] = count + inc;
+  if (count < target) {
+    counter.innerText = Math.floor(counterValues[index]);
+  } else {
+    counter.innerText = target;
+  }
+};
+
+counters.forEach((counter, index) => {
+  counterValues.push(0)
+  const interval = setInterval(() => {
+    const target = +counter.getAttribute("data-val");
+    const count = counterValues[index];
+    if (target !== count) {
+      updateCount(counter, target, count, index)
+    } else {
+      clearInterval(interval);
+    }
+  }, 1)
+});

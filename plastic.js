6ae38882617e28d01118bbb67a06535f9e2ef3fb
leuldawit <97153381+leuldawit@users.noreply.github.com>
2023-06-16 12:50:15 +0300

    filters working on plastic page

--- plastic.js
+++ plastic.js
@@ -9,70 +9,40 @@ function openNav() {
   } 
   
 
-  const observer = new IntersectionObserver(entries => {
-    // Loop over the entries
-    entries.forEach(entry => {
-      // If the element is visible
-      if (entry.isIntersecting) {
-        // Add the animation class
-        const counters = Array.from(document.querySelectorAll(".nums"));
-        const counterValues = [];
-        const speed = 40;
-        
-        const updateCount = (counter, target, count, index) => {
-          const inc = target / speed;
-          counterValues[index] = count + inc;
-          if (count < target) {
-            counter.innerText = Math.floor(counterValues[index]);
-          } else {
-            counter.innerText = target;
-          }
-        };
-        
-        counters.forEach((counter, index) => {
-          counterValues.push(0)
-          const interval = setInterval(() => {
-            const target = +counter.getAttribute("data-val");
-            const count = counterValues[index];
-            if (target !== count) {
-              updateCount(counter, target, count, index)
-            } else {
-              clearInterval(interval);
-            }
-          }, 1)
-        });
-      }
-      
-    });
-  });
   
-  observer.observe(document.querySelector('.keyFacts'));
 
-  var mySwiper = new Swiper(".swiper-container", {
-    navigation: {
-  nextEl: ".swiper-button-next",
-  prevEl: ".swiper-button-prev"
-},
-direction: "horizontal",
-loop: true,
-pagination: ".swiper-pagination",
-speed: 1200,
-paginationClickable: true,
-parallax: false,
-autoplay: {
-delay: 5000, // Delay between slides in milliseconds
-disableOnInteraction: false // Stop autoplay when user interacts with swiper
-},
-effect: "slide",
-mousewheelControl: 1,
-slidesPerView: 1.4,
-slidesPerGroup: 1
+// When the page loads, hide all machines
+//$('.machine').hide();
+
+// When a filter button is clicked
+$('.filter-btn').click(function() {
+  // Get the filter type and value from the button's data attributes
+  var filterType = $(this).data('type');
+  var filterValue = $(this).data('toggle');
+
+  // If the button is already active, remove the filter
+  if ($(this).hasClass('active')) {
+    $(this).removeClass('active');
+    filterValue = '';
+  }
+  // Otherwise, activate the filter
+  else {
+    $(this).addClass('active');
+  }
+
+  // Combine all active filters into a single selector
+  var selector = '';
+  $('.filter-btn.active').each(function() {
+    selector += $(this).data('toggle');
+  });
+
+  // If there are no active filters, show all machines
+  if (selector === '') {
+    $('.machine').show();
+  }
+  // Otherwise, filter the machines by the active filters
+  else {
+    $('.machine').hide();
+    $(selector).show();
+  }
 });
-     
-if (window.matchMedia("(max-width: 768px)and (min-width: 0px)").matches) {
-  mySwiper.params.slidesPerView = 1;
-  mySwiper.params.slidesPerGroup = 1;
-  mySwiper.update();
-}
-  
-  
